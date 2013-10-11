73e84f45c8afbe2416d4b279a0727b932a57cecd
Yiannis Yiakoumis <yiannisy@stanford.edu>
2013-10-11 10:59:38 -0700

    prepare-usb-rootfs for multiple environments.

--- utils/prepare_usb_rootfs.sh
+++ utils/prepare_usb_rootfs.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
+EXTRA_FILES="../usb-rootfs-min/opt/extra_files"
+if [ $# -lt 1 ]
+then
+    echo "Usage : $0 setup_type (dev, beta, or production)"
+    exit
+fi
 
-cp ../ap-utils/sta_manager ../usb-rootfs-min/opt/extra_files/
-cp ../ap-utils/mon_to_tap ../usb-rootfs-min/opt/extra_files/
+case "$1" in
+dev) 
+	echo "Setting up usb-rootfs-min for development environment"
+	cp ${EXTRA_FILES}/behop.dev ${EXTRA_FILES}/behop
+	;;
+beta) 
+	echo "Setting up usb-rootfs-min for beta environment"
+	cp ${EXTRA_FILES}/behop.beta ${EXTRA_FILES}/behop
+	;;
+production)
+	echo "Setting up usb-rootfs-min for production environment"
+	cp ${EXTRA_FILES}/behop.production ${EXTRA_FILES}/behop
+	;;
+*) echo "Invalid option : $@"
+	exit
+	;;
+esac
+
+cp ../ap-utils/sta_manager ${EXTRA_FILES}/
+cp ../ap-utils/mon_to_tap  ${EXTRA_FILES}/
