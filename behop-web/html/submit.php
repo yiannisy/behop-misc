ccc9a3b965726b450fab7530be31aeee72cb31ac
Yiannis Yiakoumis <yiannisy@stanford.edu>
2013-10-13 11:34:56 -0700

    Proper form, php for form processing.

---
+++ behop-web/html/submit.php
@@ -0,0 +1,20 @@
+<html>
+<head><meta http-equiv="REFRESH" content="1;url=http://www.stanford.edu/~yiannisy/cgi-bin/behop/thanks.html"></head>
+<body>
+Submitting...
+<?php
+$file = 'people.txt';
+// The new person to add to the file
+$person = $_POST["fname"];
+$person_webauth = $_ENV["REMOTE_USER"];
+$agree = isset($_POST["agree"]) && $_POST["agree"] == "yes";
+$ip = $_SERVER["REMOTE_ADDR"];
+$date = date("Y:m:d|H:i:s");
+$log = $person . "," . $person_webauth . "," . $agree . "," . $ip . "," . $date . "\n";
+// Write the contents to the file, 
+// using the FILE_APPEND flag to append the content to the end of the file
+// and the LOCK_EX flag to prevent anyone else writing to the file at the same time
+file_put_contents($file, $log, FILE_APPEND | LOCK_EX);
+?>
+</body>
+</html>
