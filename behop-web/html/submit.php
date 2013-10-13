ec88a9a494ad15bd92c0c71dc0d34117bc71ea37
Yiannis Yiakoumis <yiannisy@stanford.edu>
2013-10-13 13:46:54 -0700

    Form + several changes.

--- behop-web/html/submit.php
+++ behop-web/html/submit.php
@@ -8,9 +8,10 @@ $file = 'people.txt';
 $person = $_POST["fname"];
 $person_webauth = $_ENV["REMOTE_USER"];
 $agree = isset($_POST["agree"]) && $_POST["agree"] == "yes";
+$studio = $_POST["studio"];
 $ip = $_SERVER["REMOTE_ADDR"];
 $date = date("Y:m:d|H:i:s");
-$log = $person . "," . $person_webauth . "," . $agree . "," . $ip . "," . $date . "\n";
+$log = $person . "," . $person_webauth . "," . $studio . "," . $agree . "," . $ip . "," . $date . "\n";
 // Write the contents to the file, 
 // using the FILE_APPEND flag to append the content to the end of the file
 // and the LOCK_EX flag to prevent anyone else writing to the file at the same time
