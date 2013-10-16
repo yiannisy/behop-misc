860754de77ad7127f8e6cfb3f99ef6b7f1782038
Yiannis Yiakoumis <yiannisy@stanford.edu>
2013-10-15 17:20:24 -0700

    ignore known-hosts file for pssh/pscp.

--- utils/pi_pscp.sh
+++ utils/pi_pscp.sh
@@ -1,1 +1,1 @@
-pscp -h pi-aps.txt -l root -o /tmp/pssh-out -e /tmp/pssh-err -x "-o StrictHostKeyChecking=no" $@
+pscp -h pi-aps.txt -l root -o /tmp/pssh-out -e /tmp/pssh-err -x "-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" $@
