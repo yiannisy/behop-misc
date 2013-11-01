b73cf4b23716bf0291d5eca968ed3f8175ce1a64
Yiannis Yiakoumis <yiannisy@stanford.edu>
2013-11-01 15:07:18 -0700

    cdf-style plot for performance comparison.

---
+++ data/plot_perf.cdf.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+import string
+import sys
+import matplotlib
+matplotlib.use('Agg')
+import pylab
+#f1 = open('_snr.log','r')
+#f2 = open('_times.log','r')
+#f1_lines = f1.readlines()
+#f2_lines = f2.readlines()
+#for l1,l2 in zip(f1_lines, f2_lines):
+#    l1 = l1.strip()
+#    l2 = l2.strip()
+#    print l2[10:19], l1[-3:]
+#f1.close()
+#f2.close()
+
+output = sys.argv[1]
+
+def time_str_to_sec(timestr,base,offset=0):
+    time_vals = string.split(timestr,':')
+    time = 3600*int(time_vals[0])+60*int(time_vals[1])+int(time_vals[2])
+    time = time - base - offset
+    return time
+
+
+
+logs = [
+    ('gates-behop-complete','gates-behop'),
+    #'gates-behop-linux-complete',
+    #'gates-wrt-complete',
+    ('boonies-wrt-complete','boonies-behop'),
+    ('studio5-lwapp', 'S5-5GHz-2x2-20MHz'),
+    ('studio5-lwapp-indoors','S5-2GHz-2x2-20MHz-indoors'),
+    ('gates-netgear-behop','Gates-NetGear-behop-5GHz-2x2-20MHz'),
+    ('gates-meraki-complete','Gates-Meraki-5GHz-2x2-40MHz'),
+    ('behop-full-5ghz','Gates-Behop-5GHz-2x2-40MHz'),
+    ('gates-netgear-complete','Gates-NetGear-5GHz-2x2-40MHz'),
+
+    #'malakas_auto_dl',
+    #'malakas_no_col_auto_dl',
+    #'malakas_no_nec_auto_dl', 
+    #'malakas_no_nec_1_auto_dl',
+    #'malakas_no_nec_no_col_auto_dl',
+    #'openwrt-vanilla',
+    #'openwrt-vanilla-1',
+    #'tp_link_auto_dl',
+    #'meraki_auto_dl',
+    #'pi_meraki_auto_dl',
+    #'pi_openwrt_vanilla_auto_dl'
+    ]
+
+fig = pylab.figure(figsize=(16,12))
+lns = None
+for log in logs:
+    iperf_log = "./logs/%s.log" % log[0]
+    f = open(iperf_log,'r')
+    throughput = []
+    for line in f.readlines():
+        vals = string.split(line,',')
+        kbps = int(vals[-1])/1000
+        throughput.append(kbps)
+
+    x = sorted(throughput)
+    y = [(float(i + 1) / len(x)) for i in range(len(x))]
+    if lns:
+        lns += pylab.plot(x,y,label=log[1])
+    else:
+        lns = pylab.plot(x,y, label=log[1])
+    f.close()
+
+labels = [l.get_label() for l in lns]
+pylab.xlabel('TCP-iperf Goodput (kbps)')
+#pylab.xscale('log')
+pylab.ylabel('CDF')
+pylab.legend(lns, labels,loc=3)
+pylab.grid()
+pylab.title('WiFi Goodput (%s)' % output)
+pylab.savefig("%s.pdf" % output)
