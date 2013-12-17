3a57a24fd7b1caf390915b3e05f7ae96a0982e65
Yiannis Yiakoumis <gyiakoumis@gmail.com>
2013-12-17 03:03:38 -0800

    updates on data collection.

--- utils/analyze_tcptrace_sum.py
+++ utils/analyze_tcptrace_sum.py
@@ -1,43 +1,55 @@
 #!/usr/bin/env python
 import string,sys
 import re
+from optparse import OptionParser
 
-f = open('.tmp/tcptrace_summary.txt','r')
-for line in f.readlines()[7:]:
-    if line.find('unidirectional') != -1:
-        continue
-    p = re.compile(r'(\w+.\w+.\w+.\w+):\w+ - (\w+.\w+.\w+.\w+):\w+ \((\w+)2(\w+)\)')
-    m = p.search(line)
-    if m:
-        ip_src = m.group(1)
-        ip_dst = m.group(2)
-        flow_src = m.group(3)
-        flow_dst = m.group(4)
-        if ip_src.startswith("10.30") and ip_dst.startswith("10.30"):
-            print "SRC - DST both in network --- skipping..."
-            print ip_src,ip_dst,flow_src,flow_dst
+def analyze_tcptrace(fname_in,fname_out):
+    f = open(fname_in,'r')
+    for line in f.readlines()[7:]:
+        if line.find('unidirectional') != -1:
             continue
-        if ip_src.startswith("10.30"):
-            host = ip_src
-            end = ip_dst
-            flow = flow_dst + "2" + flow_src + "_rttraw.dat"
-        elif ip_dst.startswith("10.30"):
-            flow = flow_src + "2" + flow_dst + "_rttraw.dat"
-            host = ip_dst
-            end = ip_src
-        rtt_fname = ".tmp/" + flow
-        host_fname = '.tmp/rtt_%s.log' % host
-        try:
-            f_rtt = open(rtt_fname,'r')
-            f_host = open(host_fname,'a+')
-        except:
-            print "cannot find file (%s,%s)" % (rtt_fname,host_fname)
-            continue
-        print "Analyzing flow for %s (%s)" % (host,flow)
-        for line in f_rtt.readlines():
-            f_host.write(line)
-        f_rtt.close()
-        f_host.close()
+        p = re.compile(r'(\w+.\w+.\w+.\w+):\w+ - (\w+.\w+.\w+.\w+):\w+ \((\w+)2(\w+)\)')
+        m = p.search(line)
+        if m:
+            ip_src = m.group(1)
+            ip_dst = m.group(2)
+            flow_src = m.group(3)
+            flow_dst = m.group(4)
+            if ip_src.startswith("10.30") and ip_dst.startswith("10.30"):
+                print "SRC - DST both in network --- skipping..."
+                print ip_src,ip_dst,flow_src,flow_dst
+                continue
+            if ip_src.startswith("10.30"):
+                host = ip_src
+                end = ip_dst
+                flow = flow_dst + "2" + flow_src + "_rttraw.dat"
+            elif ip_dst.startswith("10.30"):
+                flow = flow_src + "2" + flow_dst + "_rttraw.dat"
+                host = ip_dst
+                end = ip_src
+            rtt_fname = fname_out + flow
+            host_fname = fname_out + '/rtt_%s.log' % host
+            try:
+                f_rtt = open(rtt_fname,'r')
+                f_host = open(host_fname,'a+')
+            except:
+                print "cannot find file (%s,%s)" % (rtt_fname,host_fname)
+                continue
+            print "Analyzing flow for %s (%s)" % (host,flow)
+            for line in f_rtt.readlines():
+                f_host.write(line)
+            f_rtt.close()
+            f_host.close()
         
-f.close()
-sys.exit(0)
+    f.close()
+
+if __name__== "__main__":
+
+    parser = OptionParser()
+    parser.add_option("-i", "--input-file", dest="fname_in",
+                      help="read requests from file", metavar="FILE")
+    parser.add_option("-o", "--output-file", dest="fname_out",
+                      help="write report to FILE", metavar="FILE")
+
+    (options, args) = parser.parse_args()
+    analyze_tcptrace(options.fname_in, options.fname_out)
--- utils/extract_log.sh
+++ utils/extract_log.sh
@@ -2,61 +2,120 @@
 
 pcapfile=$1
 pcapfile_out=anon_${pcapfile}
+tmp_dir=.tmp_${pcapfile}
 TCPTRACE_PATH=/home/yiannis/tcptrace-mod
 YOUTUBE_REQUESTS_ALL=/home/yiannis/be-hop-misc/data/captures/youtube_requests_all.txt
 NETFLIX_REQUESTS_ALL=/home/yiannis/be-hop-misc/data/captures/netflix_requests_all.txt
 
-mkdir .tmp
+mkdir ${tmp_dir}
 
 # ANONYMIZING
 # Split outgoing traffic and incoming traffic. Throw DNS traffic and content.
 echo "Splitting in/out, removing DNS and content..."
-time tcpdump -e -r ${pcapfile} -nnn "vlan and not port 53"  -w .tmp/studio5-out.pcap
-time tcpdump -e -r ${pcapfile} -nnn "not vlan and not port 53" -w .tmp/studio5-in.pcap
+time tcpdump -e -r ${pcapfile} -nnn "vlan and not port 53"  -w ${tmp_dir}/studio5-out.pcap
+time tcpdump -e -r ${pcapfile} -nnn "not vlan and not port 53" -w ${tmp_dir}/studio5-in.pcap
 
 # Extract youtube/netflix requests before throwing-out content.
 # We first need to strip out vlan tags for proper matching...
 echo "Stripping out vlan tags..."
-tcprewrite --enet-vlan=del --infile=.tmp/studio5-out.pcap --outfile=.tmp/studio5-out_novlan.pcap
+tcprewrite --enet-vlan=del --infile=${tmp_dir}/studio5-out.pcap --outfile=${tmp_dir}/studio5-out_novlan.pcap
 echo "Extracting youtube video requests..."
-time ngrep "algorithm=throttle-factor" -t -I .tmp/studio5-out_novlan.pcap -W byline dst port 80 | grep -v "#" | grep -E 'T |GET'  > .tmp/youtube_requests.txt
+time ngrep "algorithm=throttle-factor" -t -I ${tmp_dir}/studio5-out_novlan.pcap -W byline dst port 80 | grep -v "#" | grep -E 'T |GET'  > ${tmp_dir}/youtube_requests.txt
 echo "Extracting netflix video requests..."
-time ngrep ".ismv/range" -t -I .tmp/studio5-out_novlan.pcap -W byline dst port 80 | grep -v "#" | grep -E 'T |GET'  > .tmp/netflix_requests.txt
+time ngrep ".ismv/range" -t -I ${tmp_dir}/studio5-out_novlan.pcap -W byline dst port 80 | grep -v "#" | grep -E 'T |GET'  > ${tmp_dir}/netflix_requests.txt
 
 # Anonymize incoming/outgoing traffic
 echo "Anonymizing traffic..."
-time tcprewrite -m 54 --mtu-trunc --srcipmap=0.0.0.0/0:192.0.0.0/8 --infile=.tmp/studio5-in.pcap --outfile=.tmp/studio5-in-anon.pcap
-time tcprewrite -m 58 --mtu-trunc --dstipmap=0.0.0.0/0:192.0.0.0/8 --infile=.tmp/studio5-out.pcap --outfile=.tmp/studio5-out-anon.pcap
+time tcprewrite -m 54 --mtu-trunc --srcipmap=0.0.0.0/0:192.0.0.0/8 --infile=${tmp_dir}/studio5-in.pcap --outfile=${tmp_dir}/studio5-in-anon.pcap
+time tcprewrite -m 58 --mtu-trunc --dstipmap=0.0.0.0/0:192.0.0.0/8 --infile=${tmp_dir}/studio5-out.pcap --outfile=${tmp_dir}/studio5-out-anon.pcap
 # Merge anonymized in/out pcaps.
 echo "Merging in/out traffic..."
-time mergecap -w .tmp/${pcapfile_out} .tmp/studio5-in-anon.pcap .tmp/studio5-out-anon.pcap
+time mergecap -w ${tmp_dir}/${pcapfile_out} ${tmp_dir}/studio5-in-anon.pcap ${tmp_dir}/studio5-out-anon.pcap
 
 # ANALYSIS
 # Analyze TCP flows for RTT.
 echo "Running PDT analysis..."
-cd .tmp
+cd ${tmp_dir}
 time ${TCPTRACE_PATH}/tcptrace -nnn -r -Z ${pcapfile_out} > tcptrace_summary.txt
 cd ../ 
-python analyze_tcptrace_sum.py
+python analyze_tcptrace_sum.py -i ${tmp_dir}/tcptrace_summary.txt -o ${tmp_dir}/
 
 # Analyze video requests
 echo "Running video requests analysis..."
-[ -e .tmp/youtube_requests.txt ] && ./analyze_video_requests.py -s youtube -i .tmp/youtube_requests.txt -o .tmp/youtube.log
-[ -e .tmp/netflix_requests.txt ] && ./analyze_video_requests.py -s netflix -i .tmp/netflix_requests.txt -o .tmp/netflix.log
+[ -e ${tmp_dir}/youtube_requests.txt ] && ./analyze_video_requests.py -s youtube -i ${tmp_dir}/youtube_requests.txt -o ${tmp_dir}/youtube.log
+[ -e ${tmp_dir}/netflix_requests.txt ] && ./analyze_video_requests.py -s netflix -i ${tmp_dir}/netflix_requests.txt -o ${tmp_dir}/netflix.log
 
-# Store results
+# Store results locally
 echo "Storing results..."
-tar cvfz ${pcapfile_out}.tgz .tmp/${pcapfile_out}
+tar cvfz ${pcapfile_out}.tgz ${tmp_dir}/${pcapfile_out}
 mv ${pcapfile_out}.tgz ../data/captures/pcaps/
-[ -e .tmp/youtube.log ] && cat .tmp/youtube.log >> ../data/captures/youtube.log
-[ -e .tmp/netflix.log ] && cat .tmp/netflix.log >> ../data/captures/netflix.log
-[ -e .tmp/youtube_requests.txt ] && cat .tmp/youtube_requests.txt >> ../data/captures/youtube_requests_all.log
-[ -e .tmp/netflix_requests.txt ] && cat .tmp/netflix_requests.txt >> ../data/captures/netflix_requests_all.log
-cd .tmp/
+[ -e ${tmp_dir}/youtube.log ] && cat ${tmp_dir}/youtube.log >> ../data/captures/youtube.log
+[ -e ${tmp_dir}/netflix.log ] && cat ${tmp_dir}/netflix.log >> ../data/captures/netflix.log
+[ -e ${tmp_dir}/youtube_requests.txt ] && cat ${tmp_dir}/youtube_requests.txt >> ../data/captures/youtube_requests_all.log
+[ -e ${tmp_dir}/netflix_requests.txt ] && cat ${tmp_dir}/netflix_requests.txt >> ../data/captures/netflix_requests_all.log
+cd ${tmp_dir}/
 for i in rtt*.log;
 do 
     [ -e $i ] && cat $i >> ../../data/captures/$i
 done
 cd ../
-rm -rf .tmp
+
+# Add sums to the db.
+
+cd ${tmp_dir}
+for fname in rtt*.log;
+do
+    echo $fname
+    if [ -e $fname ]
+    then
+	tmp=${fname#rtt_}
+	ip=${tmp%.log}
+	sed -i -e "s/^/${ip} /" $fname
+	sed -i -e 's/ /,/g' $fname
+	cat $fname >> rtt_all.csv
+    fi
+done
+
+cd ../
+
+if [ -e ${tmp_dir}/netflix.log ]
+then
+    echo "timestamp,client,dest,bits,rate,dur,range" > ${tmp_dir}/netflix_db.log
+    sed 's/\//-/g' ${tmp_dir}/netflix.log >> ${tmp_dir}/netflix_db.log
+    scp ${tmp_dir}/netflix_db.log mg-xen2.stanford.edu:/tmp/
+    ssh mg-xen2.stanford.edu <<EOF
+cd /home/yiannis/behop_dashboard
+echo "Inserting Netflix logs!"
+python manage.py csvimport --mappings='' --model='logs.NetflixLog' /tmp/netflix_db.log
+EOF
+fi
+
+if [ -e ${tmp_dir}/youtube.log ]
+then
+    echo "timestamp,client,dest,bits,rate,dur,range" > ${tmp_dir}/youtube_db.log
+    sed 's/\//-/g' ${tmp_dir}/youtube.log >> ${tmp_dir}/youtube_db.log
+    scp ${tmp_dir}/youtube_db.log mg-xen2.stanford.edu:/tmp/
+    ssh mg-xen2.stanford.edu <<EOF
+cd /home/yiannis/behop_dashboard
+echo "Inserting Youtube Logs!"
+python manage.py csvimport --mappings='' --model='logs.YoutubeLog' /tmp/youtube_db.log
+EOF
+fi
+
+if [ -e ${tmp_dir}/rtt_all.csv ]
+then
+    echo "client,timestamp,seq,rtt" > ${tmp_dir}/rtt_all.log
+    cat ${tmp_dir}/rtt_all.csv >> ${tmp_dir}/rtt_all.log
+    scp ${tmp_dir}/rtt_all.log mg-xen2.stanford.edu:/tmp/
+    ssh mg-xen2.stanford.edu <<EOF
+cd /home/yiannis/behop_dashboard
+echo "Inserting RttLogs!"
+python manage.py csvimport --mappings='' --model='logs.RttLog' /tmp/rtt_all.log
+EOF
+fi
+
+ssh mg-xen2.stanford.edu "rm /tmp/netflix_db.log /tmp/youtube_db.log /tmp/rtt_all.log"
+
+rm -rf ${tmp_dir}
 rm -f ${pcapfile}
+echo "done with ${tmp_dir}"
