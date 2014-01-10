d769a136aad7138f7e4b4c2c0841dd731ca4631b
Yiannis Yiakoumis <yiannisy@stanford.edu>
2014-01-09 21:20:53 -0800

    monitoring: update script for probe detection

--- utils/update_node_db.sh
+++ utils/update_node_db.sh
@@ -26,12 +26,20 @@ sed -i 's/| /|/g' fsm.log
 sed -i 's/ (/|/g' fsm.log
 sed -i 's/)/|/g' fsm.log
 sed -i 's/, /|/g' fsm.log
+sed -i 's/ProbeReq,/ProbeReq|/g' fsm.log
 cat fsm.log | grep -E "> ASSOC|ASSOC -> NONE" | grep -v Probe | awk -F'|' '{ print $1 "|" $3 "|" $4 "|" $5}' > events.log
+cat fsm.log | grep -E "SNIFF -> RESERVED" | awk -F'|' '{ print $1 "|" $3 "|" $4 "|" $5}' >> events.log
 sed -i 's/,/./g' events.log
 sed -i 's/|/,/g' events.log
 sed -i '1itimestamp,client,event_name,signal' events.log
 
+cat .pox.log | grep PROBE_REQ | awk -F'|' '{ print $5 "," $7 }' | sort | uniq > detected.txt
+d=`date +"%Y-%m-%d %H:%M:%S%z,"`;sed -i "s/^/$d/g" detected.txt
+sed -i "s/^/DETECTED,PROBEREQ,Monitor,/g" detected.txt
+sed -i '1ievent_name,signal,category,timestamp,client,band' detected.txt
+
 cd $DASHBOARD_DIR
 python manage.py csvimport --mappings='' --model='logs.EventLog' /tmp/events.log
+python manage.py csvimport --mappings='' --model='logs.EventLog' /tmp/detected.txt
 
 cd $CURDIR
