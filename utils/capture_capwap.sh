97ee9c1e0f46f531251dfec457c5069c866f302f
Yiannis Yiakoumis <gyiakoumis@gmail.com>
2014-02-07 10:08:04 -0800

    5-min interval for capwap capture.

--- utils/capture_capwap.sh
+++ utils/capture_capwap.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-sudo tcpdump -i eth1 -nnn -s 150 -w studio6.%Y.%m%d-%H.%M.pcap -G 900 -Z yiannis -z ./extract_capwap.sh 'udp port 5247 and udp[14:2] & 0xfff8 = 0x00'
+intf=$1
+prefix=$2
+
+sudo tcpdump -i ${intf} -nnn -s 150 -w ${prefix}.%Y.%m%d-%H.%M.pcap -G 300 -Z yiannis -z ./extract_capwap.sh 'udp port 5247 and udp[14:2] & 0xfff8 = 0x00'
