52a10942e6c11f4ceb2190f3384284dc00c416b6
Yiannis Yiakoumis <yiannisy@stanford.edu>
2014-02-08 09:18:10 -0800

    better argument-passing for pi_pssh/pi_pscp

--- utils/pi_pscp.sh
+++ utils/pi_pscp.sh
@@ -1,1 +1,1 @@
-parallel-scp -h $1 -l root -o /tmp/pssh-out -e /tmp/pssh-err -x "-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" $2 $3
+parallel-scp -h $1 -l root -o /tmp/pssh-out -e /tmp/pssh-err -x "-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" ${*:2}
