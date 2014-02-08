f4e67fa894703fd91b6408f7c8911ae28159bd2a
Yiannis Yiakoumis <yiannisy@stanford.edu>
2014-02-08 09:18:41 -0800

    argument passing for pi_pscp/pi_pssh

--- utils/update_usb.sh
+++ utils/update_usb.sh
@@ -1,13 +1,13 @@
 echo `date` ":"  "Mounting usb drive..."
-./pi_pssh.sh -t 10 "mount /dev/sda1 /mnt/"
+./pi_pssh.sh $1 -t 10  "mount /dev/sda1 /mnt/"
 echo `date` ":"  "Cleanin-up USB drive."
-./pi_pssh.sh -t 10 "rm -rf /mnt/*"
+./pi_pssh.sh $1 -t 10  "rm -rf /mnt/*"
 echo `date` ":"  "Copying usb image over..."
-./pi_pscp.sh -r -t 20 ../usb-rootfs-min/* /mnt/
+./pi_pscp.sh $1 -r -t 20 ../usb-rootfs-min/* /mnt/
 echo `date` ":"  "Unmounting usb drive..."
-./pi_pssh.sh -t 10 "umount /dev/sda1"
+./pi_pssh.sh $1 -t 10 "umount /dev/sda1"
 echo `date` ":"  "Rebooting into usb mode."
-./pi_pssh.sh -t 10 "/sbin/pi_check_mount_usb"
+./pi_pssh.sh $1 -t 10 "/sbin/pi_check_mount_usb"
 #echo `date` ":"  "Waiting for reboot..."
 #sleep 90
 #echo `date` ":"  "Setting up usb-filesystem. When this completes, the system will reboot on usb mode."
