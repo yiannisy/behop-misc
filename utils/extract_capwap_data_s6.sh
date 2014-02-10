eec729daedcd1be71e72a7083f9b05d6ed68ad58
Yiannis Yiakoumis <gyiakoumis@gmail.com>
2014-02-10 15:13:05 -0800

    s6-specific naming for log scripts.

---
+++ utils/extract_capwap_data_s6.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+file=$1
+
+# filter first frame packets only
+tcpdump -nnn -r ${file} 'udp[14:2] & 0xfff8 = 0x00' -w dl_f0.pcap
+# Filter packets with capwap.header.length=2 and remove CAPWAP and WiFi crap
+tcpdump -nnn -r dl_f0.pcap 'udp[9:1] & 0xf8 = 0x10 and udp[17:1] = 0x08' -w test_l2.pcap
+bittwiste -I test_l2.pcap -O test_l2_data.pcap -D 1-68
+# Filter packets with capwap.header.length=4 and remove CAPWAP and WiFi crap
+tcpdump -nnn -r dl_f0.pcap 'udp[9:1] & 0xf8 = 0x20 and udp[25:1] = 0x08' -w test_l4.pcap
+bittwiste -I test_l4.pcap -O test_l4_data.pcap -D 1-76
+
+# Create a unified pcap file
+mergecap test_l2_data.pcap test_l4_data.pcap -w test_data.pcap
+
+echo "Created data stream for studio5 lwapp!"
