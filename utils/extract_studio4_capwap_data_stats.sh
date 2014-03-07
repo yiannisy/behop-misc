805b123912b1b4d70b82ff2fcc08b5900c82974d
Yiannis Yiakoumis <gyiakoumis@gmail.com>
2014-03-06 22:21:36 -0800

    fix path.

--- utils/extract_studio4_capwap_data_stats.sh
+++ utils/extract_studio4_capwap_data_stats.sh
@@ -2,6 +2,7 @@
 
 N_ARGS=4
 f_out="/tmp/cargus_out.txt"
+PATH=/usr/local/bin/:${PATH}
 
 if [ $# -ne "$N_ARGS" ]
 then
@@ -19,7 +20,7 @@ fi
 day="$YY.$MM.$DD-$HH:59:00"
 date_prefix=$YY.$MM.$DD.$HH
 cd /var/log/cargus/archive/$YY/$MM/$DD/
-rabins -r *${date_prefix}* -M 60m hard -m saddr - ipv4 and tcp and src net 10.30.0.0/16 -w - | rasort -r - -m saddr -s saddr dpkts spkts dbytes sbytes | tr -s ' ' > ${f_out}
+rabins -r *${date_prefix}* -M 60m hard -m saddr daddr - tcp and src or dst net 10.30.0.0/16 -w - | rasort -r - -m saddr daddr -s saddr daddr dpkts spkts dbytes sbytes | tr -s ' ' > ${f_out}
 sed -i '/SrcAddr/d' ${f_out}
 sed -i 's/^ *//g' ${f_out}
 sed -i "s/^/${day},/g" ${f_out}
@@ -27,5 +28,5 @@ sed -i 's/ /,/g' ${f_out}
 sed -i "s/^/S4,/g" ${f_out}
 sed -i "1ilocation,@timestamp,client,in_pkts,out_pkts,in_bytes,out_bytes" ${f_out}
 cd -
-./add_csv_to_db_direct.sh ${f_out} logs_transferlog
-rm ${f_out}
+#./add_csv_to_db_direct.sh ${f_out} logs_transferlog
+#rm ${f_out}
