7b352d858ac2847d2d9d14da0853c9071d9eab5a
Yiannis Yiakoumis <gyiakoumis@gmail.com>
2014-03-07 17:43:27 -0800

    Netflix + Youtube extraction for S5.

--- utils/extract_youtube.sh
+++ utils/extract_youtube.sh
@@ -24,29 +24,29 @@ ARGUS_DIR=$ARGUS_PREFIX/$date
 UTIL_DIR=/home/yiannis/be-hop-misc/utils/
 
 
-# Get all the netflix flows for this day.
-cat /home/yiannis/be-hop-misc/data/captures/netflix_requests_all.log | grep $date | awk -F'[ :]' '{ print $6,$9 }' | sort | uniq -c | sort -n > /tmp/netflix_flows.txt
+# Get all the youtube flows for this day.
+cat /home/yiannis/be-hop-misc/data/captures/youtube_requests_all.log | grep $date | awk -F'[ :]' '{ print $6,$9 }' | sort | uniq -c | sort -n > /tmp/youtube_flows.txt
 
 my_pcap_expr="host 172.24.74.179"
 cd $ARGUS_DIR
 echo "#################"
-echo "Processing netflix for $YY-$MM-$DD ($LOC)"
+echo "Processing Youtube for $YY-$MM-$DD ($LOC)"
 while read p;do
     host_src=`echo $p | awk -F' ' '{ print $2 }'`
     #port_src=`echo $p | awk -F' ' '{ print $3 }'`
     host_dst=`echo $p | awk -F' ' '{ print $3 }'`
     pcap_expr="$my_pcap_expr or (host $host_src and host $host_dst)"
     my_pcap_expr=$pcap_expr
-done < /tmp/netflix_flows.txt
+done < /tmp/youtube_flows.txt
 
 if [[ -z "$pcap_expr" ]]
 then
-    echo "no netflix flow detected."
+    echo "no youtube flow detected."
     exit
 else
     echo "processing argus data for $pcap_expr"
 fi
-fname=/tmp/netflix_${day}.txt
+fname=/tmp/youtube_${day}.txt
 rabins -r * -M 1m hard -m saddr daddr - $pcap_expr -w - | rasort -r - -m stime -s stime saddr daddr sload dload | tr -s ' ' > ${fname}
 cp ${fname} ${fname}.orig
 sed -i '/StartTime/d' ${fname}
@@ -66,7 +66,6 @@ if [ "$lines" -gt "$thres" ];then
 	#sed -i "${lines}d" ${fname}
     sed -i '1ilocation,@timestamp,client,target,bitrate_up,bitrate_down' ${fname}
     cd ${UTIL_DIR}/
-    ./add_csv_to_db_direct.sh ${fname} logs_netflixbitratelog
+    ./add_csv_to_db_direct.sh ${fname} logs_youtubebitratelog
     cd -
 fi
-#done < /tmp/netflix_flows.txt
