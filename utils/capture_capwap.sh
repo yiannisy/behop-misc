81f2ad35dcd77dfc91118898452c9fe8136ef773
Yiannis Yiakoumis <gyiakoumis@gmail.com>
2014-03-12 15:02:44 -0700

    Capture traffic to/from local APs only.

--- utils/capture_capwap.sh
+++ utils/capture_capwap.sh
@@ -2,5 +2,13 @@
 
 intf=$1
 prefix=$2
+lwapp_ap_prefix="172.19.159"
+pcap_expr="host 172.19.159.20"
 
-tcpdump -i ${intf} -nnn -s 150 -w ${prefix}.%Y.%m%d-%H.%M.pcap -G 300 -z ./extract_capwap.sh 'udp port 5247 and udp[14:2] & 0xfff8 = 0x00'
+for i in {21..45};do
+    pcap_expr="$pcap_expr or host $lwapp_ap_prefix.$i"
+done
+pcap_expr="( $pcap_expr )"
+echo $pcap_expr
+
+tcpdump -i ${intf} -nnn -s 150 -w ${prefix}.%Y.%m%d-%H.%M.pcap -G 300 -z ./extract_capwap.sh "udp port 5247 and udp[14:2] & 0xfff8 = 0x00 and $pcap_expr"
--- utils/capture_capwap_video.sh
+++ utils/capture_capwap_video.sh
@@ -3,4 +3,13 @@
 intf=$1
 prefix=$2
 
-tcpdump -i ${intf} -nnn -s 1000 -w ${prefix}.%Y.%m%d-%H.%M.pcap -G 300 -z ./extract_capwap_video.sh 'udp dst port 5247 and udp[14:2] & 0xfff8 = 0x00'
+lwapp_ap_prefix="172.19.159"
+pcap_expr="host 172.19.159.20"
+
+for i in {21..45};do
+    pcap_expr="$pcap_expr or host $lwapp_ap_prefix.$i"
+done
+pcap_expr="( $pcap_expr )"
+echo $pcap_expr
+
+tcpdump -i ${intf} -nnn -s 1000 -w ${prefix}.%Y.%m%d-%H.%M.pcap -G 300 -z ./extract_capwap_video.sh "udp dst port 5247 and udp[14:2] & 0xfff8 = 0x00 and $pcap_expr"
