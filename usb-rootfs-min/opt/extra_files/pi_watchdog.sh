26ab779a3e3613143093fcfe58b5c9d0041665da
Yiannis Yiakoumis <yiannisy@stanford.edu>
2014-03-17 10:53:21 -0700

    More conservative watchdog.

--- usb-rootfs-min/opt/extra_files/pi_watchdog.sh
+++ usb-rootfs-min/opt/extra_files/pi_watchdog.sh
@@ -1,10 +1,11 @@
 tries=0
-while [[ $tries -lt 10 ]]
+while [[ $tries -lt 60 ]]
 do
     if ping -c 1 8.8.8.8 >/dev/null
     then
 	exit 0
     fi
+    sleep 3
     tries=$((tries+1))
 done
 date=`date`
