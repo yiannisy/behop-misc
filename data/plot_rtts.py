66e3eaeac6f3e7fb053c10eff4d7eb0d042ed6c5
Yiannis Yiakoumis <gyiakoumis@gmail.com>
2014-03-20 14:42:39 -0700

    Plot rtt updates.

--- data/plot_rtts.py
+++ data/plot_rtts.py
@@ -7,32 +7,62 @@ from util import plot_cdf
 
 s4_rtts = defaultdict(list)
 s4_path="./rtt-analysis/studio4_rtts/"
+s4_path="./rtt-2014-03-16/"
 s5_rtts = defaultdict(list)
-s5_path="./rtt-analysis/studio5_rtts/"
+s5_path="./rtt-2014-03-18/"
 
 to_plot = []
 files = os.listdir(s4_path)
+files = [f for f in files if f.startswith("rtt_10.30")]
 for fname in files:
     fname = s4_path + fname
     f = open(fname,'r')
-    for l in f.readlines():
-        ip,vals = string.split(l.rstrip(),',')
-        ts,seq,rtt = string.split(vals,' ')
-        s4_rtts[ip].append(float(rtt)/1000.0)
-    to_plot.append([s4_rtts[ip],'S4','g'])
+    line = f.readlines()[0]
+    ip,vals = string.split(line.rstrip(),',')
+    ts,seq,rtt = string.split(vals,' ')
+    ts = float(ts)
+    ts_start = ts
+    lines = f.readlines()
+    while True:
+        while ts < ts_start + 60:
+            if len(lines) == 0:
+                break
+            l = lines.pop(0)
+            ip,vals = string.split(l.rstrip(),',')
+            ts,seq,rtt = string.split(vals,' ')
+            ts = float(ts)
+            s4_rtts[ip].append(float(rtt)/1000.0)
+        to_plot.append([s4_rtts[ip],'Multiple-Channels','g'])
+        ts_start = ts
+        if len(lines) == 0:
+            break
 
 files = os.listdir(s5_path)
+files = [f for f in files if f.startswith("rtt_10.30")]
 for fname in files:
     fname = s5_path + fname
     f = open(fname,'r')
-    for l in f.readlines():
-        ip,vals = string.split(l.rstrip(),',')
-        ts,seq,rtt = string.split(vals,' ')
-        s5_rtts[ip].append(float(rtt)/1000.0)
-    to_plot.append([s5_rtts[ip],'S5','r'])
+    line = f.readlines()[0]
+    ip,vals = string.split(line.rstrip(),',')
+    ts,seq,rtt = string.split(vals,' ')
+    ts = float(ts)
+    ts_start = ts
+    lines = f.readlines()
+    while True:
+        while ts < ts_start + 60:
+            if len(lines) == 0:
+                break
+            l = lines.pop(0)
+            ip,vals = string.split(l.rstrip(),',')
+            ts,seq,rtt = string.split(vals,' ')
+            ts = float(ts)
+            s5_rtts[ip].append(float(rtt)/1000.0)
+        to_plot.append([s5_rtts[ip],'Single-Channel','r'])
+        ts_start = ts
+        if len(lines) == 0:
+            break
 
-
-plot_cdf(to_plot,'rtts',legend=False,xlogscale=True,title='RTTs',xlabel='time (ms)',ylabel='CDF',alpha=0.5,xlim=[0.1,10**5])
+plot_cdf(to_plot,'rtts',legend=False,xlogscale=True,title='RTTs',xlabel='PDT (ms)',ylabel='CDF',alpha=0.5,xlim=[0.1,10**5])
 
 print files
 sys.exit(0)
